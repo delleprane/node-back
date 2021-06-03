9767698a2fa5f2f11ff7ab50f367627c8850fbb0
unknown <fernandadelleprane@gmail.com>
2021-06-03 13:25:49 -0300

    rota de procura por nome de usuario

--- routes/filter.routes.js
+++ routes/filter.routes.js
@@ -5,12 +5,27 @@ const mongoose = require('mongoose');
 
 const User = require("../models/user.model");
 
-
+//Filter por nickname
 search.post('/search', async(req, res) => {
     try {
         const { nickname } = req.body
         const userSearching = await User.find({
             nickname: { $regex: '.*' + nickname + '.*' }
+        })
+        res.status(200).json({ userSearching })
+    } catch (error) {
+        res.status(500).json({ message: "Erro ao buscar usuário" })
+    }
+
+});
+
+
+//Filter por name
+search.post('/search/name', async(req, res) => {
+    try {
+        const { name } = req.body
+        const userSearching = await User.find({
+            name: { $regex: '.*' + name + '.*' }
         })
         res.status(200).json({ userSearching })
     } catch (error) {
